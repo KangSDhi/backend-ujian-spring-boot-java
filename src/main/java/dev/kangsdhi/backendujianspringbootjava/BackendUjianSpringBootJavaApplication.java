835edb3f55ce37bfe405289a2a3e957bd2a56801
KangSDhi <kangteknisi@gmail.com>
2024-06-14 21:01:28 +0700

    first commit

---
+++ src/main/java/dev/kangsdhi/backendujianspringbootjava/BackendUjianSpringBootJavaApplication.java
@@ -0,0 +1,13 @@
+package dev.kangsdhi.backendujianspringbootjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendUjianSpringBootJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendUjianSpringBootJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/kangsdhi/backendujianspringbootjava/BackendUjianSpringBootJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.kangsdhi.backendujianspringbootjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendUjianSpringBootJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
